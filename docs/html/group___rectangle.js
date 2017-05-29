43a8d878ea1e856b84f832e2442b828598503bed
jneubrand <johannes_n@icloud.com>
2017-05-29 14:36:47 -0700

    Fix doxygen folder structure

--- docs/html/group___rectangle.js
+++
@@ -1,6 +0,0 @@
-var group___rectangle =
-[
-    [ "n_graphics_draw_rect", "group___rectangle.html#gacef9280620581f1e0ea20f9a726877c0", null ],
-    [ "n_graphics_draw_thin_rect", "group___rectangle.html#gab83794b8bd998584f7b0f2409cc3752b", null ],
-    [ "n_graphics_fill_rect", "group___rectangle.html#ga3dc4844c266b84f2959efb3f2031a019", null ]
-];
